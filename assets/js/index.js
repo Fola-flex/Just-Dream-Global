b23ba29ef9fc9d69bae98e2d02b63c2da249d5b9
Fola-flex <91006932+Fola-flex@users.noreply.github.com>
2022-08-28 15:53:42 +0100

    created new pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,12 +10,12 @@ function shownav() {
     
     if (list.classList.contains('sidenav')) {
         navbtn.innerHTML = '&times;';
-        navbtn.style.fontSize = '55px';
-        navbtn.style.top = '2%';
+        navbtn.style.fontSize = '35px';
+
     } else {
         navbtn.innerHTML = '&#9776;';
         navbtn.style.fontSize = '25px';
-        navbtn.style.top = '3%';
+        
     }
 };
 
